435ff8cae5b68c7a4e0f14cf4b2bc42b10a0bb9e
Eva <ekoss.prog@gmail.com>
2024-03-25 13:31:51 +0400

    Update Amazon Shipping Calculator JavaScript functions:
    Improved input handling by clearing total cost and error message elements before calculations
    Added validation to prevent cost less than  and display an error message
    Adjusted cost calculation to add  shipping for orders under

---
+++ scripts/amazon-shipping-calc.js
@@ -0,0 +1,24 @@
+function calculateCostKeydown(event) {
+    if (event.key === 'Enter') calculateCost();
+}
+
+function calculateCost () {
+    const inputElement = document.querySelector('.js-input-element');
+    let cost = Number(inputElement.value);
+
+    document.querySelector('.js-total-cost')
+      .innerHTML = '';
+    document.querySelector('.js-error-message')
+      .innerHTML = '';
+
+    if (cost < 0) {
+      document.querySelector('.js-error-message')
+        .innerText = 'Error: cost cannot be less than $0';
+        return
+    } 
+      
+    if (cost < 40) cost += 10;
+    
+    document.querySelector('.js-total-cost')
+      .innerText = `$${cost}`;
+}
